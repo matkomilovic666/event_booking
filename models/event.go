4888e48f693c485f9f0d72817ca5b2b03b167a99
matkomilovic666 <milovichurtado@gmail.com>
2025-06-04 17:44:14 -0400

    Setup an event model for the API

---
+++ models/event.go
@@ -0,0 +1,20 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string
+	Description string
+	Location    string
+	DateTime    time.Time
+	UserID      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	// TODO: add it to a db
+
+	events = append(events, e)
+}
