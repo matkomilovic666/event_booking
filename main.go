886cc8bc0052014c988367fa364a3f2ac8450d01
matkomilovic666 <milovichurtado@gmail.com>
2025-06-04 17:36:51 -0400

    Setting up /events route using GIN and handling a first GET event

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080") //localhost:8080 for dev
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 }
