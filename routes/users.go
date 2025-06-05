100142119a2b01f5de96a38981cdc16ee94b6e9d
matkomilovic666 <milovichurtado@gmail.com>
2025-06-05 00:15:27 -0400

    Created the users table and the API to sign up a new user into the database

---
+++ routes/users.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"event-booking/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request data."})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created!", "user": user})
+}
