d9d6166670882701cf09b886a10ad52632216052
matkomilovic666 <milovichurtado@gmail.com>
2025-06-04 20:48:33 -0400

    Register a POST method to create an event, add test files and save the events

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"event-booking/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +11,27 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080 for dev
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request data."})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
