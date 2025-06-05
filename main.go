4a1af7101956feab9a1142638bd98fa6d62993fa
matkomilovic666 <milovichurtado@gmail.com>
2025-06-04 21:07:31 -0400

    Create an events table and add the db logic to main

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"event-booking/db"
 	"event-booking/models"
 	"net/http"
 
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
