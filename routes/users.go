214bf1bf3760903a56012a7e46ba8c1d75484c39
matkomilovic666 <milovichurtado@gmail.com>
2025-06-12 15:08:25 -0400

    Implement the jwt generation logic using the email and user id

--- routes/users.go
+++ routes/users.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"event-booking/models"
+	"event-booking/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -42,5 +43,12 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 }
