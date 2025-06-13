5421bf9193ee820b46e2325abb56eb3bcd1fcb57
matkomilovic666 <milovichurtado@gmail.com>
2025-06-12 20:17:23 -0400

    Add a middleware to authenticare the POST, PUT, and DELETE events

--- routes/events.go
+++ routes/events.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"event-booking/models"
-	"event-booking/utils"
 	"net/http"
 	"strconv"
 
@@ -38,27 +37,15 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request data."})
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 	err = event.Save()
 
